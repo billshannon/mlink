b707b26976d60328d4df8516bb445efc98ec0153
Bill Shannon <bill@billshannon.com>
2015-09-28 11:12:02 -0600

    change hardcoded plan to params:id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @basic_plan = Plan.find(1)
-    @pro_plan = Plan.find(2)
-  end
+    # @basic_plan = Plan.find(1)
+    # @pro_plan = Plan.find(2)
+    @basic_plan = Plan.find(params[:id])
+    @pro_plan = Plan.find(params[:id])
 
+  end
   def about
   end
 end
