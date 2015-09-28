bf1ab541139417769c3f4e8e11f9a6b0c8ffcb79
Bill Shannon <bill@billshannon.com>
2015-09-28 11:35:34 -0600

    reverted to hardcoded plans and update database on heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def home
-    # @basic_plan = Plan.find(1)
-    # @pro_plan = Plan.find(2)
-    @basic_plan = Plan.find(params[:id])
-    @pro_plan = Plan.find(params[:id])
-
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   def about
   end
